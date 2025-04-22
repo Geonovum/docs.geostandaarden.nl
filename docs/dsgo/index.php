c237b08b6feb820af4271b7f1b36685cbca6883e
pasibun <matthijs_hovestad@hotmail.com>
2025-04-22 10:55:44 +0200

    test data

---
+++ docs/dsgo/index.php
@@ -0,0 +1,5 @@
+<?php
+header( "Status: 301 Moved Permanently" );
+header( "Location: https://docs.geostandaarden.nl/dsgo/vv-hr-DSGO-20220316/" );
+exit(0); 
+?>
---
+++ docs/imgeo/vv-wa-IMGeo-20230321/media/colofon.js
@@ -0,0 +1,27 @@
+var respecConfig = {
+  specStatus: "GN-VV",
+  specType: "WA",
+  //canonicalURI: "WV",
+  publishDate: "2023-03-21",
+  editors: [    
+    {
+      name: "Geonovum",      
+      mailto: "imgeo@geonovum.nl",    
+    }    
+  ],
+  shortName: "IMGeo",
+  pubDomain: "IMGeo",
+  //previousURI: "https://www.geonovum.nl/BRO",
+  github: "https://github.com/Geonovum/IMGeo",
+  previousPublishDate: "2022-09-04",
+  previousStatus: "GN-VV",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+};
