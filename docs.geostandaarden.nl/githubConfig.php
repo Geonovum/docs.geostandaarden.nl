21f640693377d4b35846ffc5fb07b5ebe77c5230
pasibun <matthijs_hovestad@hotmail.com>
2025-07-24 11:18:21 +0200

    prod copy

--- docs.geostandaarden.nl/githubConfig.php
+++
@@ -1,5 +0,0 @@
-﻿<?php
-$pubDomainListURL = 'https://raw.githubusercontent.com/Geonovum/respec-utils/master/src/autodeploy/config/pubDomainList.json';
-// configure this in GitHub's webhook
-$hubSecret = 'Geostandaarden2018';
-?>
